72c26cc6e03eeba4eb901b5e471384978f05062f
vagrant <vagrant@homestead>
2018-11-08 09:05:32 +0000

    商品后台添加类目字段

--- app/Admin/Controllers/CategoriesController.php
+++ app/Admin/Controllers/CategoriesController.php
@@ -78,7 +78,7 @@ class CategoriesController extends Controller
                 ->rules('required');
 
             // 定义一个名为父类目的下拉框
-            $form->select('parent_id', '父类目')->ajax('/admin/api/categories');
+            $form->select('parent_id', '父类目')->ajax('/admin/api/categories?is_directory=0');
         }
 
         return $form;
@@ -90,7 +90,7 @@ class CategoriesController extends Controller
         // 用户输入的值通过 q 参数获取
         $search = $request->input('q');
         $result = Category::query()
-            ->where('is_directory', true)  // 由于这里选择的是父类目，因此需要限定 is_directory 为 true
+            ->where('is_directory', boolval($request->input('is_directory', true)))  // 由于这里选择的是父类目，因此需要限定 is_directory 为 true
             ->where('name', 'like', '%'.$search.'%')
             ->paginate();
 
