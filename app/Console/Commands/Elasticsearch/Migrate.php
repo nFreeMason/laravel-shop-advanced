f59f4bb6f457d29c9c9572e83cbfa8c30b5beb1a
nFreeMason <1502534146@qq.com>
2019-01-21 18:52:59 +0800

    'travis'

--- app/Console/Commands/Elasticsearch/Migrate.php
+++ app/Console/Commands/Elasticsearch/Migrate.php
@@ -48,15 +48,84 @@ class Migrate extends Command
 	protected function createIndex($aliasName, $indexClass)
 	{
 		// todo
+		// 调用 create() 方法创建索引
+		$this->es->indices()->create([
+			// 第一个版本的索引名后缀为 _0
+			'index' => $aliasName.'_0',
+			'body'  => [
+				// 调用索引类的 getSettings() 方法获取索引设置
+				'settings' => $indexClass::getSettings(),
+				'mappings' => [
+					'_doc' => [
+						// 调用索引类的 getProperties() 方法获取索引字段
+						'properties' => $indexClass::getProperties(),
+					],
+				],
+				'aliases'  => [
+					// 同时创建别名
+					$aliasName => new \stdClass(),
+				],
+			],
+		]);
 	}
 
 	protected function updateIndex($aliasName, $indexClass)
 	{
 		// todo
+		// 暂时关闭索引
+		$this->es->indices()->close(['index' => $aliasName]);
+		// 更新索引设置
+		$this->es->indices()->putSettings([
+			'index' => $aliasName,
+			'body'  => $indexClass::getSettings(),
+		]);
+		// 更新索引字段
+		$this->es->indices()->putMapping([
+			'index' => $aliasName,
+			'type'  => '_doc',
+			'body'  => [
+				'_doc' => [
+					'properties' => $indexClass::getProperties(),
+				],
+			],
+		]);
+		// 重新打开索引
+		$this->es->indices()->open(['index' => $aliasName]);
 	}
 
 	protected function reCreateIndex($aliasName, $indexClass)
 	{
 		// todo
+		// 获取索引信息，返回结构的 key 为索引名称，value 为别名
+		$indexInfo     = $this->es->indices()->getAliases(['index' => $aliasName]);
+		// 取出第一个 key 即为索引名称
+		$indexName = array_keys($indexInfo)[0];
+		// 用正则判断索引名称是否以 _数字 结尾
+		if (!preg_match('~_(\d+)$~', $indexName, $m)) {
+			$msg = '索引名称不正确:'.$indexName;
+			$this->error($msg);
+			throw new \Exception($msg);
+		}
+		// 新的索引名称
+		$newIndexName = $aliasName.'_'.($m[1] + 1);
+		$this->info('正在创建索引'.$newIndexName);
+		$this->es->indices()->create([
+			'index' => $newIndexName,
+			'body'  => [
+				'settings' => $indexClass::getSettings(),
+				'mappings' => [
+					'_doc' => [
+						'properties' => $indexClass::getProperties(),
+					],
+				],
+			],
+		]);
+		$this->info('创建成功，准备重建数据');
+		$indexClass::rebuild($newIndexName);
+		$this->info('重建成功，准备修改别名');
+		$this->es->indices()->putAlias(['index' => $newIndexName, 'name' => $aliasName]);
+		$this->info('修改成功，准备删除旧索引');
+		$this->es->indices()->delete(['index' => $indexName]);
+		$this->info('删除成功');
 	}
 }
--- app/Http/Controllers/Test.php
+++ app/Http/Controllers/Test.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UserAddressRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class Test extends Controller
 {
     //
-	public function index(UserAddressRequest $request)
+	public function index(User $user)
 	{
-
+		$user = $user->find(1);
+		$user->money += 888;
+		$user->save();
+		dd($user);
 	}
 }
